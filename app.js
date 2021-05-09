f01854f4a0bf4dde932fc3a8d71b1f4f67251c4d
KimDaeSeon <kimdaeseon@khu.ac.kr>
2021-05-10 00:10:35 +0900

    naver.js의 함수 분리
    Former-commit-id: 7ef6bf17553165d119c593fec623c7de6863d8ef

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express')
-const puppeteer = require('puppeteer')
+
+const naverRouter = require('./router/naver.js')
+
 
 const app = express();
+
 const server = app.listen(3000,()=>{
     const host = server.address().address
     const port = server.address().port
     console.log("app listening at http://%s:%s", host, port)
+})
+
+app.use('/naver', naverRouter)
+
+app.get('/naver', (req, res)=>{
+    res.send("error")
 })
 
 app.get('/', (req, res)=>{
