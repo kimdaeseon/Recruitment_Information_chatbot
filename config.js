47590b697aad405db70fb60c996254fc86807e68
김대선 <kimdaeseon@khu.ac.kr>
2021-06-05 20:41:14 +0900

    Delete config.js
    Former-commit-id: 34e7b6acc4ce41e90bad7c0640729d5f1ea72eef

--- config.js
+++
@@ -1,8 +0,0 @@
-
-const domain = "www.kimdaeseonpractice.gq"
-const TOKEN = "+BXJA8tXmpuFplEUDBTpj/7l2YbtBbFLelQ4rQnJ6QhIj1FxqvCUUYilQ0PJOGUL0te2tPzx2VjlDAf+GuYVENNr6mIIkLXSTk2eKk/MOKPzR33sW6zlKk7U+eaT8QhK3szXg4Q4Bj5HQXeYcIMapgdB04t89/1O/w1cDnyilFU="
-
-module.exports={
-    domain : domain,
-    TOKEN : TOKEN
-}
